ed0487a6576f40d7e18d98a36e764cf752517a34
Andre Cardoso <andrecardosodev@gmail.com>
2013-02-06 23:38:01 -0200

    Removida validação de data

--- src/app/Model/Receita.php
+++ src/app/Model/Receita.php
@@ -12,24 +12,6 @@ class Receita extends AppModel {
  * @var array
  */
 	public $validate = array(
-		'data' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 		'valor' => array(
 			'notempty' => array(
 				'rule' => array('notempty'),
