c0b85b0b91cfb20bf9fcff9abfe9a078120f9e70
Andre Cardoso <andrecardosodev@gmail.com>
2013-02-17 21:18:44 -0300

    Criado Cron para redimensionamento de imagens

---
+++ src/app/Controller/Cron.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Realiza diversas tarefas em background no servidor
+ *
+ * 
+ * @require Imagick
+ * 
+ * @author andre
+ */
+class Cron {
+    
+    public static function task() {
+        try{
+            // redimensionando todas as imagens
+            exec('mogrify -resize 128 *.jpg');
+            exec('mogrify -resize 128 *.png');
+        } catch (Exception $e) {
+            mail('andrecardosodev@gmail.com','Erro no cron Ehoje', $e->getMessage());
+        } 
+    }
+    
+}
+
+Cron::task();
+
+?>
